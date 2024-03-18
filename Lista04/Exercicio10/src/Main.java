6e9082557ceccb795042faa4f6822497aa9a6eff
llei7e <leitellucas@gmail.com>
2024-03-18 16:24:56 -0300

    Exercicios 1 ao 11 Concluidos

--- Lista04/Exercicio10/src/Main.java
+++ Lista04/Exercicio10/src/Main.java
@@ -1,16 +1,29 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         int[][] a = {{2, 3},
-                     {4, 5,},
+                     {4, 5},
                      {6, 7}};
         int[][] b = {{8, 9},
                      {10, 11}};
-        int[][] pro = new int[2][3];
+        int[][] pro = new int[3][2];
 
-        for (int i = 0; i < a[i].length; i++) {
-            for (int j = 0; j < a.length; j++) {
 
+
+        for (int z = 0; z < b[0].length; z++) {
+            for (int i = 0; i < a[0].length; i++) {
+                for (int j = 0; j < a.length; j++) {
+                    if (i == 0) {
+                        pro[j][z] = a[j][i] * b[i][z];
+                    } else if (i == 1) {
+                        pro[j][z] = pro[j][z] + (a[j][i] * b[i][z]);
+                    }
+                }
             }
+        }
+        for (int[] ver :pro){
+            System.out.println(Arrays.toString(ver));
         }
     }
 }
